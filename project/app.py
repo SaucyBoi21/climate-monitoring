53f62d31208b96bfdbdb1ba1606f593a3dafa92f
SaucyBoi21 <72944967+SaucyBoi21@users.noreply.github.com>
2022-11-10 19:41:28 -0400

    added flask functionalities

--- project/app.py
+++ project/app.py
@@ -3,6 +3,11 @@
 import time
 from bme280 import BME280
 
+from flask import Flask
+
+app = Flask(__name__)
+
+
 try:
     from smbus2 import SMBus
 except ImportError:
@@ -24,25 +29,7 @@ Press Ctrl+C to exit!
 bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
 
-count = 0
-values = {
-
-}
-
-
-def add_value(temp, pres, hum):
-    value = {
-        "temperature": temp,
-        "pressure": pres,
-        "humidity": hum
-    }
-    values.append(value)
-
-
-while True:
+@app.get("/temperature")
+def get_temperature():
     temperature = bme280.get_temperature()
-    pressure = bme280.get_pressure()
-    humidity = bme280.get_humidity()
-    add_value(temperature, pressure, humidity)
-    print(values)
-    time.sleep(1)
+    return temperature
